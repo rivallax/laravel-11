0fa6beb1622dabafede37d56923ae926a2b7a829
rivallax <2223606.rivaldi@smkn-2sbg.sch.id>
2024-11-27 21:36:33 +0700

    first commit

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>shopp  - ralavall</title>
+    <style>
+        .text-gradient {
+            font-weight: bold; /* Teks lebih tebal */
+            background: linear-gradient(90deg, #ff7eb3, #ff758c, #fd5f00);
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+            text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3); /* Efek bayangan */
+            font-size: 2rem; /* Ukuran font lebih besar */
+        }
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="background: lightgray">
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h3 class="text-center my-4 fst-italic text-gradient">PROJECT LARAVEL 11 - DASAR</h3>
+                    <h5 class="text-center"><a href="https://santrikoding.com">https://github.com/rivallax/laravel-11.com</a></h5>
+                    <hr>
+                </div>
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <a href="{{ route('products.create') }}" class="btn btn-md btn-success mb-3">ADD PRODUCT</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">IMAGE</th>
+                                    <th scope="col">TITLE</th>
+                                    <th scope="col">PRICE</th>
+                                    <th scope="col">STOCK</th>
+                                    <th scope="col" style="width: 20%">ACTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($products as $product)
+                                    <tr>
+                                        <td class="text-center">
+                                            <img src="{{ asset('/storage/products/'.$product->image) }}" class="rounded" style="width: 150px">
+                                        </td>
+                                        <td>{{ $product->title }}</td>
+                                        <td>{{ "Rp " . number_format($product->price,2,',','.') }}</td>
+                                        <td>{{ $product->stock }}</td>
+                                        <td class="text-center">
+                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('products.destroy', $product->id) }}" method="POST">
+                                                <a href="{{ route('products.show', $product->id) }}" class="btn btn-sm btn-dark">SHOW</a>
+                                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-primary">EDIT</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">HAPUS</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <div class="alert alert-danger">
+                                        Data Products belum Tersedia.
+                                    </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        {{ $products->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        //message with sweetalert
+        @if(session('success'))
+            Swal.fire({
+                icon: "success",
+                title: "BERHASIL",
+                text: "{{ session('success') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        @elseif(session('error'))
+            Swal.fire({
+                icon: "error",
+                title: "GAGAL!",
+                text: "{{ session('error') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        @endif
+
+    </script>
+
+</body>
+</html>
